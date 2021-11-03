5e16425750878a98f571eb67146cc201f0391682
Gabriel Galvan <gabgh@hotmail.com>
2021-11-03 07:45:26 -0500

    modificacion de serverjs con documentacion

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const cors = require("cors");
+//crear el servidor
+const app = express();
 
-app.get("/", (require, response) => {
-  console.log("raiz de proyect");
-  response.json({
-    message: "peticion localhost:3000",
-    status: "ok",
-  });
-});
+//CORS
+app.use(cors());
 
+//lectura de body
+app.use(express.json());
+//
+const router = require("./routes/auth.routes");
+//GET
+// app.get("/", (req, res) => {
+//   res.json({
+//     message: "peticion localhost:3000",
+//     status: "ok",
+//   });
+// });
+
+//implementacion de las rutas
+app.use("/api/auth-t-evolvers", router);
+//Port app
 app.listen(3000, () => {
   console.log("server listen in port ", 3000);
 });
